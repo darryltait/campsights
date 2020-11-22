b3f424659461eee66110e711ba200897cfdd6b4d
darrylt <4dtait@gmail.com>
2020-11-22 15:22:55 -0700

    fix reviews display issue

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -24,23 +24,25 @@ function show(req,res) {
             campers.forEach(function(camper){
                 camper.reviews.forEach(function(rev){
                     //if(rev.campgroundId == campground._id){
-                        if(rev.campgroundId == '5fb8794c4df31baeca70b223'){
+                        //if(rev.campgroundId == '5fb8794c4df31baeca70b223'){
+                        if(rev.campgroundId == req.params.id){
 
                         console.log(rev.campgroundId); //will print the campgroundId
+                        console.log('reqparamsid', req.params.id);
                         //console.log(campground._id);  // prints the first campgroundID = 5fb876954df31baeca70b222
                         //console.log(req.params.id);  // prints the above ^
                         reviewsArr.push(rev);
 
                         //console.log(reviewsArr)
 
-                    } else {
-                        //reviewsArr.push(rev);
-                        //console.log(reviewsArr)
+                    } //else {
+                    //     reviewsArr.push(rev);
+                    //     console.log(reviewsArr)
 
-                    }
+                    // }
                 }) 
                 
-                console.log(reviewsArr);
+                //console.log(reviewsArr);
             }); 
             res.render('campgrounds/show', {
                 campground,

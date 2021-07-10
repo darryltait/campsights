8bc54df880c171345bf4e310c2e44f4f885b00c5
Darryl Tait <4dtait@gmail.com>
2021-07-10 16:34:12 -0600

    replace background image for speed

--- models/review.js
+++ models/review.js
@@ -1,12 +1,14 @@
 // require our modules
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // create a shortcut variable
 const Schema = mongoose.Schema;
 
 // define commentSchema
-const reviewSchema = new Schema({
+const reviewSchema = new Schema(
+  {
     text: String,
-    campground: {type: Schema.Types.ObjectId, ref: 'Campground'},
-    createdBy: {type: Schema.Types.ObjectId, ref: 'Camper'},
-}, 
-    {timestamps: true});
+    campground: { type: Schema.Types.ObjectId, ref: "Campground" },
+    createdBy: { type: Schema.Types.ObjectId, ref: "Camper" },
+  },
+  { timestamps: true }
+);
